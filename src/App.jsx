6885b9b5fcf00d2282fe49a5746352b8aaac1f28
Emilse Arias <emilsearias@gmail.com>
2024-02-07 10:09:19 -0300

    agregamos el componente Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 function App() {
-const [personaje, setPersonaje] = useState({});
+  const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
-useEffect(()=>{
-  consultarAPI();
-}, []);
+  useEffect(() => {
+    consultarAPI();
+  }, []);
 
-const consultarAPI = async()=>{
-  //hacer una peticion get a la api
-  const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
-  const datos = await respuesta.json();
-  console.log(datos[0]);
-  setPersonaje(datos[0])
-}
+  const consultarAPI = async () => {
+    //hacer una peticion get a la api
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const datos = await respuesta.json();
+    console.log(datos[0]);
+    setPersonaje(datos[0]);
+    //ocultar el spinner
+    setMostrarSpinner(false);
+  };
+
+  const mostrarComponente = mostrarSpinner ? (<div className="my-4">
+  <Spinner animation="border" variant="light"></Spinner>
+</div>) : ( <Frase personaje={personaje}></Frase>);
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase personaje={personaje}></Frase>
-        <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
+       {mostrarComponente}
+        <Button variant="warning" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
       </Container>
     </>
   );
