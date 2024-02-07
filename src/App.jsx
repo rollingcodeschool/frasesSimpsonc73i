e2751a2ee0e6ef5290178df961f272c816460e66
Emilse Arias <emilsearias@gmail.com>
2024-02-07 10:23:57 -0300

    agregamos un try catch en la consulta

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,37 @@ function App() {
   }, []);
 
   const consultarAPI = async () => {
-    //mostrar el spinner
-    setMostrarSpinner(true);
-    //hacer una peticion get a la api
-    const respuesta = await fetch(
-      "https://thesimpsonsquoteapi.glitch.me/quotes"
-    );
-    const datos = await respuesta.json();
-    console.log(datos[0]);
-    setPersonaje(datos[0]);
-    //ocultar el spinner
-    setMostrarSpinner(false);
+    try {
+      //mostrar el spinner
+      setMostrarSpinner(true);
+      //hacer una peticion get a la api
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      const datos = await respuesta.json();
+      console.log(datos[0]);
+      setPersonaje(datos[0]);
+      //ocultar el spinner
+      setMostrarSpinner(false);
+    } catch (error){
+      console.log(error);
+      //agregar un mensaje para el usuario final
+    }
   };
 
-  const mostrarComponente = mostrarSpinner ? (<div className="my-4">
-  <Spinner animation="border" variant="light"></Spinner>
-</div>) : ( <Frase personaje={personaje}></Frase>);
+  const mostrarComponente = mostrarSpinner ? (
+    <div className="my-4">
+      <Spinner animation="border" variant="light"></Spinner>
+    </div>
+  ) : (
+    <Frase personaje={personaje}></Frase>
+  );
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-       {mostrarComponente}
+        {mostrarComponente}
         <Button variant="warning" onClick={consultarAPI}>
           Obtener frase
         </Button>
